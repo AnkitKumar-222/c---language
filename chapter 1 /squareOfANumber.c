e02c30616c4bb9b33e166f9804f4b0617a22f0a2
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-24 12:22:27 +0000

    Square of a number

---
+++ chapter 1 /squareOfANumber.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    int x;
+    printf("Enter a number: ");
+    scanf("%d",&x); //& -> ampercent
+    printf("The square is: ");
+    printf("%d",x*x);
+
+    return 0;
+}
