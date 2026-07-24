4147fde4f0bbc65518417a92ef9b98acd5881dec
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-24 10:21:47 +0000

    updation

---
+++ basics/variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    // this is a comment
+    // this is helpful to take notes
+
+    // how to print numbers
+    // printf("this is how I print text\n");
+    // printf("%d",123);
+
+    printf("123+2");
+    printf("\n");
+    printf("%d", 123*2);
+
+    return 0;
+}
