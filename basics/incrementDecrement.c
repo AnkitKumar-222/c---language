d81b1d51208d106a0a4d5c33b8430e6cf355146a
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-24 11:46:46 +0000

    Area of circle

--- basics/incrementDecrement.c
+++ basics/incrementDecrement.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 int main(){
 
-    int x = 54;
+    // int x = 54;
+    // printf("%d\n",x);
+    // x++; // x = x + 1; // x = 55
+    // printf("%d\n",x);
+    // x = x * 8 ; // x = 440
+    // printf("%d\n",x);
+    // x--; // x = 439
+    // printf("%d\n",x);
+
+    int x = 5;
     printf("%d\n",x);
-    x++; // x = x + 1; // x = 55
+    printf("%d\n",++x);
     printf("%d\n",x);
-    x = x * 8 ; // x = 440
-    printf("%d\n",x);
-    x--; // x = 439
-    printf("%d\n",x)
+    // printf("%d\n",x);
+    // printf("%d\n",x++);
+    // printf("%d\n",x);
 
     return 0 ;
 
