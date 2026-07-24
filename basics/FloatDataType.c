b058db6aff3244e58e780a2fa631c575af75c815
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-24 10:53:47 +0000

    float data type

--- basics/FloatDataType.c
+++ basics/FloatDataType.c
@@ -1,10 +1,23 @@
+// #include<stdio.h>
+// int main(){
+//     float x = 5;
+//     float y = 2;
+//     float z = 5.0/2.0;
+
+//     printf("%f",z);
+    
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main(){
-    float x = 5;
+    float x = 7;
     float y = 2;
-    float z = 5.0/2.0;
-
-    printf("%f",z);
+    printf("%f\n",x+y);
+    printf("%f\n",x-y);
+    printf("%f\n",x*y);
+    printf("%f\n",x/y);
     
     return 0;
 }
--- basics/updationOfVariables.c
+++ basics/updationOfVariables.c
@@ -4,9 +4,14 @@ int main(){
     printf("%d\n",x);
     x = 10 ;
     printf("%d\n",x);
-    x = x + 100;
+    x = x + 100;    // x = 110
     printf("%d\n",x); 
-    x = x - 105;
+    x = x - 105;    // x = 5
+    printf("%d\n",x);
+    x = x * 2;  // x = 10
+    printf("%d\n",x);
+    x = x/5;    // x = 2
     printf("%d\n",x);
     return 0;
+
 }
