cbfeb4d7802e6da2b99b4884dc079e786d3f8fb0
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-30 10:29:50 +0000

    half of number

---
+++ chapter 1 /halfOfNumber.c
@@ -0,0 +1,7 @@
+#include<stdio.h>       
+int main(){
+    float X;
+    printf("enter a number : ");
+    scanf("%f",x/2);
+    printf("%f",x/2);
+}
---
+++ chapter 1 /modulus.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x = 343;
+    int y = 13;
+    int z = x%y;
+    printf("%d",z );
+}
