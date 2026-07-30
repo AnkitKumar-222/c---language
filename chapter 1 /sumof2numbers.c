602d416acec4cf0169aab71536d6fec5764f5f34
Ankit kumar <naudiyalankit22@outlook.com>
2026-07-30 10:13:05 +0000

    Sum of  2 numbers

---
+++ chapter 1 /sumof2numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("enter first number : ");
+    scanf("%d",&x); // input 
+    int y; 
+    printf("enter second number : ");
+    scanf("%d",&y);
+    printf("The sum is : %d",x+y);
+}
